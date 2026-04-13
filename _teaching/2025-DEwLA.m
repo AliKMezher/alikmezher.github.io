ea6e7b5dc6ff4abd4362fc98b54b47a4ab3c102c
Ali Mezher <alimezher@utexas.edu>
2026-04-13 09:49:22 -0500

    Update venue and location for DEwLA course

--- _teaching/2025-DEwLA.m
+++ _teaching/2025-DEwLA.m
@@ -3,7 +3,7 @@ title: "Differential Equations with Linear Algebra"
 collection: teaching
 type: "Undergraduate Course"
 permalink: /teaching/2025-DEwLA.m
-venue: "Online Private Course."
+venue: "Department of Mathematics/ University of Texas at Austin "
 date: 2025-06-01 
-location: " Department of Mathematics/ University of Texas at Austin"
+location: " USA "
 ---
