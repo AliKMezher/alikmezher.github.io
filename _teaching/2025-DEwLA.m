72e58d6218e900b51970b70e11332570a95ac45d
Ali Mezher <alimezher@utexas.edu>
2026-04-13 09:46:04 -0500

    Update permalink for Differential Equations course

---
+++ _teaching/2025-DEwLA.m
@@ -0,0 +1,9 @@
+---
+title: "Differential Equations with Linear Algebra"
+collection: teaching
+type: "Undergraduate Course"
+permalink: /teaching/2025-DEwLA.m
+venue: "Online Private Course."
+date: 2025-06-01 
+location: " Department of Mathematics/ University of Texas at Austin"
+---
